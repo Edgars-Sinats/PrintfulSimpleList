00c15a6e011bec23af83f749b665f7c2626fe6b0
Edgars-Sinats <sinedgars@gmail.com>
2021-02-25 22:37:41 +0200

    Initial commit, NewsApi fech working!

---
+++ app/src/main/java/com/example/printfulsimplelist/APIRequest.kt
@@ -0,0 +1,18 @@
+package com.example.printfulsimplelist
+
+import com.example.printfulsimplelist.api.NewsApiJSON
+import retrofit2.http.GET
+
+interface APIRequest {
+
+    /**
+     Searching after documentation in
+     https://newsapi.org/docs/endpoints
+
+     Optional, example:
+     https://newsapi.org/v2/everything?q=bitcoin&apiKey=04a42ee08c094f9580d3cb6e0bd5a43c
+     */
+
+    @GET("/v2/top-headlines?country=lv&apiKey=04a42ee08c094f9580d3cb6e0bd5a43c")
+    suspend fun getNews() : NewsApiJSON
+}
---
+++ app/src/main/java/com/example/printfulsimplelist/api/Source.kt
@@ -0,0 +1,6 @@
+package com.example.printfulsimplelist.api
+
+data class Source(
+    val id: Any,
+    val name: String
+)
